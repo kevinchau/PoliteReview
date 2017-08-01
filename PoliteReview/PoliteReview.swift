2dfc82d80768c0c9a7bee8640c86857be98080f2
Kevin Chau <chaukevin@gmail.com>
2017-08-01 12:44:33 +0800

    Initial Commit

---
+++ PoliteReview/PoliteReview.swift
@@ -0,0 +1,84 @@
+//
+//  PoliteReview.swift
+//  PoliteReview
+//
+//  Created by Kevin Chau on 8/1/17.
+//  Copyright © 2017 likelylabs. All rights reserved.
+//
+
+import Foundation
+
+
+internal enum UserDefaultKeys: String {
+    case PoliteReviewTotalLaunchCount
+    case PoliteReviewVersionLaunchCount
+    case PoliteReviewReviewedVersion
+    case PoliteReviewHasReviewed
+}
+
+public class PoliteReview {
+    
+    static private(set) internal var storeID: String = ""
+    static private(set) internal var contactURL: String = ""
+    static private(set) internal var contactEmail: String = ""
+    static private(set) var triggerCount: Int = 5
+    
+    public func setup(itunesID: String, contactURL: String, contactEmail: String, triggerCount: Int = 5) {
+        PoliteReview.storeID = itunesID
+        PoliteReview.contactURL = contactURL
+        PoliteReview.contactEmail = contactEmail
+        incrementTotalLaunch()
+        incrementVersionLaunch()
+    }
+    
+    private func incrementTotalLaunch() {
+        // Keep track of how many times the app has been launched
+        let totalLaunchCount = UserDefaults.standard.integer(forKey: UserDefaultKeys.PoliteReviewTotalLaunchCount.rawValue)
+        UserDefaults.standard.set(totalLaunchCount + 1, forKey: UserDefaultKeys.PoliteReviewTotalLaunchCount.rawValue)
+        UserDefaults.standard.synchronize()
+    }
+    
+    private func incrementVersionLaunch() {
+        let currentVersion = getVersion()
+        
+        // Reset the counter if the current app version is newer than a previous reviewed version
+        if let storedVersion = UserDefaults.standard.string(forKey: UserDefaultKeys.PoliteReviewReviewedVersion.rawValue) {
+            if currentVersion.compare(storedVersion, options: NSString.CompareOptions.numeric) != ComparisonResult.orderedDescending {
+                UserDefaults.standard.set(0, forKey: UserDefaultKeys.PoliteReviewVersionLaunchCount.rawValue)
+            }
+        }
+        
+        // Keep track of how many times this particular version has been launched
+        let currentCount = UserDefaults.standard.integer(forKey: UserDefaultKeys.PoliteReviewVersionLaunchCount.rawValue)
+        UserDefaults.standard.set(currentCount + 1, forKey: UserDefaultKeys.PoliteReviewVersionLaunchCount.rawValue)
+        UserDefaults.standard.synchronize()
+    }
+    
+    internal func shouldPresent() -> Bool {
+        // Determines if a review should be presented based on a new version and if the user has been prompted before
+        var should = false
+        let currentVersion = getVersion()
+        let hasReviewed = UserDefaults.standard.bool(forKey: UserDefaultKeys.PoliteReviewHasReviewed.rawValue)
+        let versionCount = UserDefaults.standard.integer(forKey: UserDefaultKeys.PoliteReviewVersionLaunchCount.rawValue)
+        
+        if versionCount >= PoliteReview.triggerCount {
+            if let storedVersion = UserDefaults.standard.string(forKey: "reviewVersion") {
+                if currentVersion.compare(storedVersion, options: NSString.CompareOptions.numeric) == ComparisonResult.orderedDescending {
+                    should = true
+                }
+            }
+            else if hasReviewed == false {
+                should = true
+            }
+        }
+        return should
+    }
+    
+    internal func getVersion() -> String {
+        // Get current app version string
+        guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return "0.0.0"
+        }
+        return version
+    }
+}
---
+++ PoliteReview/RequestPoliteReview.swift
@@ -0,0 +1,121 @@
+//
+//  RequestPoliteReview.swift
+//  PoliteReview
+//
+//  Created by Kevin Chau on 8/1/17.
+//  Copyright © 2017 likelylabs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import StoreKit
+import MessageUI
+
+public class RequestPoliteReview: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    public func request() {
+        // Presents a UIAlertController for the initial prompt
+        let appName = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"]
+        
+        let alertController = UIAlertController(title: String("\(NSLocalizedString("Do you love", comment: "For the alert controller preceding the app name")) \(appName ?? "this app")?"), message: NSLocalizedString("Give Us Your Feedback", comment: "Message for the alert controller"), preferredStyle: .alert)
+        
+        // They love it action
+        let action1 = UIAlertAction(title: NSLocalizedString("Yes", comment: "Alert Action"), style: .default) { (UIAlertAction) in
+            
+            // Sets the reviewed status and sets the reviewed version
+            self.userDefaults()
+            
+            // Requests a review via the StoreKit alert, or legacy if 10.3 is not available
+            if #available(iOS 10.3, *) {
+                SKStoreReviewController.requestReview()
+            } else {
+                self.legacyAlert()
+            }
+        }
+        
+        // They Don't love it action
+        let action2 = UIAlertAction(title: NSLocalizedString("No", comment: "Alert Action"), style: .cancel) { (UIAlertAction) in
+            // Sets the reviewed status and sets the reviewed version
+            self.userDefaults()
+            // Initiates the contact process
+            self.contact()
+        }
+        
+        // Adds the actions to the alert controller
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        
+        if PoliteReview().shouldPresent() == true {
+            present(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    private func legacyAlert() {
+        // Legacy alert prompts user to write a review if they are below 10.3
+        let legacyAlertController = UIAlertController(title: NSLocalizedString("Thank You!", comment: "Message when user love app"), message: NSLocalizedString("Please Consider Writing a Review", comment: "Ask legacy user for review"), preferredStyle: .alert)
+        // Action to write a review
+        let legacyAction1 = UIAlertAction(title: NSLocalizedString("Write Review", comment: "Alert Action"), style: .default) { (UIAlertAction) in
+            let openAppStoreForRating = "itms-apps://itunes.apple.com/us/app/id\(PoliteReview.storeID)?action=write-review&mt=8"
+            if UIApplication.shared.canOpenURL(URL(string: openAppStoreForRating)!) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(URL(string: openAppStoreForRating)!, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(URL(string: openAppStoreForRating)!)
+                }
+            }
+        }
+        // Action to not write a review
+        let legacyAction2 = UIAlertAction(title: NSLocalizedString("Not Now", comment: "Alert Action"), style: .cancel, handler: nil)
+        // Adds the actions to the alert controller
+        legacyAlertController.addAction(legacyAction1)
+        legacyAlertController.addAction(legacyAction2)
+        
+        present(legacyAlertController, animated: true, completion: nil)
+    }
+    
+    private func contact() {
+        let alertController = UIAlertController(title: NSLocalizedString("Oh No!", comment: "Alert title when user does not love your app"), message: NSLocalizedString("Let us know how to improve.", comment: "Message when user does not love your app"), preferredStyle: .alert)
+        let action1 = UIAlertAction(title: NSLocalizedString("Send Feedback", comment: "alert action"), style: .default) { (UIAlertAction) in
+            let appName = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"]
+            if MFMailComposeViewController.canSendMail() {
+                let composeVC = MFMailComposeViewController()
+                composeVC.mailComposeDelegate = self
+                
+                // Configure the fields of the interface.
+                composeVC.setToRecipients([PoliteReview.contactEmail])
+                composeVC.setSubject("Contact from \(appName ?? "iOS App")")
+                composeVC.setMessageBody("Message: ", isHTML: false)
+                
+                // Present the view controller modally.
+                self.present(composeVC, animated: true, completion: nil)
+            } else {
+                guard let url = URL(string: PoliteReview.contactURL) else {
+                    return
+                }
+                if UIApplication.shared.canOpenURL(url){
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+        }
+        let action2 = UIAlertAction(title: NSLocalizedString("Not Now", comment: "Alert Action"), style: .cancel) { (UIAlertAction) in
+            // stuff here
+        }
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func userDefaults() {
+        // Sets the reviewed status and sets the reviewed version
+        let currentVersion = PoliteReview().getVersion()
+        UserDefaults.standard.set(true, forKey: UserDefaultKeys.PoliteReviewHasReviewed.rawValue)
+        UserDefaults.standard.set(currentVersion, forKey: UserDefaultKeys.PoliteReviewReviewedVersion.rawValue)
+        UserDefaults.standard.synchronize()
+    }
+    
+}
