e766563baaf89c1faa66130944f8cfd02a6b8906
Kevin Chau <chaukevin@gmail.com>
2017-08-01 10:43:10 +0800

    Initial Commit

---
+++ PoliteReview/PoliteReview.h
@@ -0,0 +1,19 @@
+//
+//  PoliteReview.h
+//  PoliteReview
+//
+//  Created by Kevin Chau on 8/1/17.
+//  Copyright © 2017 likelylabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PoliteReview.
+FOUNDATION_EXPORT double PoliteReviewVersionNumber;
+
+//! Project version string for PoliteReview.
+FOUNDATION_EXPORT const unsigned char PoliteReviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PoliteReview/PublicHeader.h>
+
+
