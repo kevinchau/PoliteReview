c279b834b204634ca262472a8b7850e1b7d7e599
Kevin Chau <chaukevin@gmail.com>
2017-08-02 09:56:13 +0800

    Forces use of Singleton - less verbose, and less risk of requesting review before things are set up.

--- PoliteReview/RequestPoliteReview.swift
+++ PoliteReview/RequestPoliteReview.swift
@@ -20,10 +20,10 @@ extension UIViewController: MFMailComposeViewControllerDelegate {
         let alertController = UIAlertController(title: String("\(NSLocalizedString("Do you love", comment: "For the alert controller preceding the app name")) \(appName ?? "this app")?"), message: NSLocalizedString("Give Us Your Feedback", comment: "Message for the alert controller"), preferredStyle: .alert)
         
         // They love it action
-        let action1 = UIAlertAction(title: NSLocalizedString("Yes", comment: "Alert Action"), style: .default) { (UIAlertAction) in
+        let yesAction = UIAlertAction(title: NSLocalizedString("Yes", comment: "Alert Action"), style: .default) { (UIAlertAction) in
             
             // Sets the reviewed status and sets the reviewed version
-            self.userDefaults()
+            self.setUserDefaultsHasReviewed()
             
             // Requests a review via the StoreKit alert, or legacy if 10.3 is not available
             if #available(iOS 10.3, *) {
@@ -34,18 +34,18 @@ extension UIViewController: MFMailComposeViewControllerDelegate {
         }
         
         // They Don't love it action
-        let action2 = UIAlertAction(title: NSLocalizedString("No", comment: "Alert Action"), style: .cancel) { (UIAlertAction) in
+        let noAction = UIAlertAction(title: NSLocalizedString("No", comment: "Alert Action"), style: .cancel) { (UIAlertAction) in
             // Sets the reviewed status and sets the reviewed version
-            self.userDefaults()
+            self.setUserDefaultsHasReviewed()
             // Initiates the contact process
             self.contact()
         }
         
         // Adds the actions to the alert controller
-        alertController.addAction(action1)
-        alertController.addAction(action2)
+        alertController.addAction(yesAction)
+        alertController.addAction(noAction)
         
-        if PoliteReview().shouldPresent() == true {
+        if PoliteReview.main.shouldPresent() == true {
             self.present(alertController, animated: true, completion: nil)
         }
         
@@ -55,7 +55,7 @@ extension UIViewController: MFMailComposeViewControllerDelegate {
         // Legacy alert prompts user to write a review if they are below 10.3
         let legacyAlertController = UIAlertController(title: NSLocalizedString("Thank You!", comment: "Message when user love app"), message: NSLocalizedString("Please Consider Writing a Review", comment: "Ask legacy user for review"), preferredStyle: .alert)
         // Action to write a review
-        let legacyAction1 = UIAlertAction(title: NSLocalizedString("Write Review", comment: "Alert Action"), style: .default) { (UIAlertAction) in
+        let legacyWriteAction = UIAlertAction(title: NSLocalizedString("Write Review", comment: "Alert Action"), style: .default) { (UIAlertAction) in
             let openAppStoreForRating = "itms-apps://itunes.apple.com/us/app/id\(PoliteReview.storeID)?action=write-review&mt=8"
             if UIApplication.shared.canOpenURL(URL(string: openAppStoreForRating)!) {
                 if #available(iOS 10.0, *) {
@@ -66,17 +66,17 @@ extension UIViewController: MFMailComposeViewControllerDelegate {
             }
         }
         // Action to not write a review
-        let legacyAction2 = UIAlertAction(title: NSLocalizedString("Not Now", comment: "Alert Action"), style: .cancel, handler: nil)
+        let legacyDeclineAction = UIAlertAction(title: NSLocalizedString("Not Now", comment: "Alert Action"), style: .cancel, handler: nil)
         // Adds the actions to the alert controller
-        legacyAlertController.addAction(legacyAction1)
-        legacyAlertController.addAction(legacyAction2)
+        legacyAlertController.addAction(legacyWriteAction)
+        legacyAlertController.addAction(legacyDeclineAction)
         
         present(legacyAlertController, animated: true, completion: nil)
     }
     
     private func contact() {
         let alertController = UIAlertController(title: NSLocalizedString("Oh No!", comment: "Alert title when user does not love your app"), message: NSLocalizedString("Let us know how to improve.", comment: "Message when user does not love your app"), preferredStyle: .alert)
-        let action1 = UIAlertAction(title: NSLocalizedString("Send Feedback", comment: "alert action"), style: .default) { (UIAlertAction) in
+        let contactAction = UIAlertAction(title: NSLocalizedString("Send Feedback", comment: "alert action"), style: .default) { (UIAlertAction) in
             let appName = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"]
             if MFMailComposeViewController.canSendMail() {
                 let composeVC = MFMailComposeViewController()
@@ -102,19 +102,19 @@ extension UIViewController: MFMailComposeViewControllerDelegate {
                 }
             }
         }
-        let action2 = UIAlertAction(title: NSLocalizedString("Not Now", comment: "Alert Action"), style: .cancel) { (UIAlertAction) in
+        let declineContactAction = UIAlertAction(title: NSLocalizedString("Not Now", comment: "Alert Action"), style: .cancel) { (UIAlertAction) in
             // stuff here
         }
-        alertController.addAction(action1)
-        alertController.addAction(action2)
+        alertController.addAction(contactAction)
+        alertController.addAction(declineContactAction)
         self.present(alertController, animated: true, completion: nil)
     }
     
-    private func userDefaults() {
+    private func setUserDefaultsHasReviewed() {
         // Sets the reviewed status and sets the reviewed version
-        let currentVersion = PoliteReview().getVersion()
-        UserDefaults.standard.set(true, forKey: UserDefaultKeys.PoliteReviewHasReviewed.rawValue)
-        UserDefaults.standard.set(currentVersion, forKey: UserDefaultKeys.PoliteReviewReviewedVersion.rawValue)
+        let currentVersion = PoliteReview.main.getVersion()
+        UserDefaults.standard.set(true, forKey: UserDefaultKeys.politeReviewHasReviewed.rawValue)
+        UserDefaults.standard.set(currentVersion, forKey: UserDefaultKeys.politeReviewReviewedVersion.rawValue)
         UserDefaults.standard.synchronize()
     }
     
